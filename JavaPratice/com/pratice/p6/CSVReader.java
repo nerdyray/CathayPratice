5b6247f967566275798dfbf6137df74f2aff6cab
ray <nerdyray317@gmail.com>
2025-12-03 10:28:42 +0800

    Refactor CSVReader and orderByManu classes: replace Comparator with lambda expressions for sorting

--- JavaPratice/com/pratice/p6/CSVReader.java
+++ JavaPratice/com/pratice/p6/CSVReader.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,31 +14,42 @@ public class CSVReader {
     //建立Read方法，讓carsList可以從外部存取
     public static List<Map<String, Object>> read() throws FileNotFoundException {
         File getCSVFile = new File("d:/CathayPratice/JavaPratice/cars1.csv");
-        Scanner sc = new Scanner(getCSVFile);
-        String headerLine = sc.nextLine();
-        String[] headers = headerLine.split(",");
-        List<Map<String, Object>> cars = new ArrayList<>();
+        try (Scanner sc = new Scanner(getCSVFile)) {
+            String headerLine = sc.nextLine();
+            String[] headers = headerLine.split(",");
+            List<Map<String, Object>> cars = new ArrayList<>();
 
-        while (sc.hasNext()) {
-            String line = sc.nextLine();
-            String[] values = line.split(",");
-            Map<String, Object> map = new HashMap<>();
-            for (int i = 0; i < values.length; i++) {
-                map.put(headers[i], values[i]);
+            while (sc.hasNext()) {
+                String line = sc.nextLine();
+                String[] values = line.split(",");
+                Map<String, Object> map = new HashMap<>();
+                for (int i = 0; i < values.length; i++) {
+                    map.put(headers[i], values[i]);
+                }
+                cars.add(map);
             }
-            cars.add(map);
-        }
-        Collections.sort(cars, new Comparator<Map<String, Object>>() {
-            @Override
-            public int compare(Map<String, Object> a, Map<String, Object> b) {
-                Object priceAObj = a.get("Price");
-                Object priceBObj = b.get("Price");
-                BigDecimal priceA = new BigDecimal(priceAObj.toString());
-                BigDecimal priceB = new BigDecimal(priceBObj.toString());
+            Collections.sort(cars, (a, b) -> {
+                BigDecimal priceA = new BigDecimal(a.get("Price").toString());
+                BigDecimal priceB = new BigDecimal(b.get("Price").toString());
                 return priceA.compareTo(priceB);
-            }
-        });
-        return cars;
 
+            });
+            sc.close();
+            return cars;
+        }
     }
 }
+
+/*
+        Collections.sort(cars, new Comparator<Map<String, Object>>() {
+            @Override
+        public int compare(Map<String, Object> a, Map<String, Object> b) {
+        Object priceAObj = a.get("Price");
+        Object priceBObj = b.get("Price");
+        BigDecimal priceA = new BigDecimal(priceAObj.toString());
+        BigDecimal priceB = new BigDecimal(priceBObj.toString());
+        return priceA.compareTo(priceB);
+        }
+        });
+        改寫成lambda語法
+ */
--- JavaPratice/com/pratice/p6/orderByManu.java
+++ JavaPratice/com/pratice/p6/orderByManu.java
@@ -2,7 +2,6 @@ package com.pratice.p6;
 
 import java.math.BigDecimal;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -12,28 +11,39 @@ public class orderByManu {
         String prevManu = null;
         BigDecimal sumPrice = BigDecimal.ZERO;
         BigDecimal sumMin = BigDecimal.ZERO;
-        Collections.sort(carsList, new Comparator<Map<String, Object>>() {
-            @Override
-            public int compare(Map<String, Object> a, Map<String, Object> b) {
-                String manuA = a.get("manufacturer").toString();
-                String manuB = b.get("manufacturer").toString();
-                int result = manuA.compareTo(manuB);
-                if (result != 0) {
-                    return result;
-                }
-                Object priceAObj = a.get("Price");
-                Object priceBObj = b.get("Price");
-                BigDecimal priceA = new BigDecimal(priceAObj.toString());
-                BigDecimal priceB = new BigDecimal(priceBObj.toString());
-                return priceA.compareTo(priceB);
+        Collections.sort(carsList, (a, b) -> {
+            String manuA = a.get("manufacturer").toString();
+            String manuB = b.get("manufacturer").toString();
+            int result = manuA.compareTo(manuB);
+            if (result != 0) {
+                return result;
             }
-        }
-        );
+            BigDecimal priceA = new BigDecimal(a.get("Price").toString());
+            BigDecimal priceB = new BigDecimal(b.get("Price").toString());
+            return priceA.compareTo(priceB);
+        });
+        // Collections.sort(carsList, new Comparator<Map<String, Object>>() {
+        //     @Override
+        //     public int compare(Map<String, Object> a, Map<String, Object> b) {
+        //         String manuA = a.get("manufacturer").toString();
+        //         String manuB = b.get("manufacturer").toString();
+        //         int result = manuA.compareTo(manuB);
+        //         if (result != 0) {
+        //             return result;
+        //         }
+        //         Object priceAObj = a.get("Price");
+        //         Object priceBObj = b.get("Price");
+        //         BigDecimal priceA = new BigDecimal(priceAObj.toString());
+        //         BigDecimal priceB = new BigDecimal(priceBObj.toString());
+        //         return priceA.compareTo(priceB);
+        //     }
+        // }
+        // );
         System.out.printf("%-15s %-10s %-12s %-10s\n",
                 "Manufacturer", "TYPE", "Min.PRICE", "Price");
         BigDecimal totalMin = BigDecimal.ZERO;
         BigDecimal totalPrice = BigDecimal.ZERO;
-        for (Map m : carsList) {
+        for (Map<String, Object> m : carsList) {
             String currentManu = m.get("manufacturer").toString();
             BigDecimal price = new BigDecimal(m.get("Price").toString());
             BigDecimal min = new BigDecimal(m.get("Min.PRICE").toString());
