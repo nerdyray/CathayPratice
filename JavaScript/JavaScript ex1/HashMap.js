330749f134acb89c58001911076adb0e65135133
ray <nerdyray317@gmail.com>
2025-12-08 16:42:12 +0800

    Refactor HashMap.js; improve key validation and add clear functionality

--- JavaScript/JavaScript ex1/HashMap.js
+++ JavaScript/JavaScript ex1/HashMap.js
@@ -4,6 +4,8 @@ let key = [
 let value = [
 
 ];
+//建立流水號
+let nextNum = 0;
 //新增KEY / VALUE
 document.getElementById("PUT").addEventListener("click", function () {
     const KEY = document.getElementById("KEY").value;
@@ -13,16 +15,23 @@ document.getElementById("PUT").addEventListener("click", function () {
     console.log(key, value);
 
 });
+document.getElementById("CLEAR").addEventListener("click", function () {
+    Result.innerHTML = '';
+
+});
 
 function HashMap(KEY, VALUE) {
-    if (KEY.trim() === '') {
-        alert('KEY值不能為空');
+    if (KEY.trim() === '' || key.includes(KEY)) {
+        alert('KEY值不能為空或已有重複KEY值');
+        return;
     }
     key.push(KEY);
     value.push(VALUE);
 }
 
 function Render() {
+    //清空舊畫面
+    Result.innerHTML = '';
     //CreateTitle
     const resultText =
         document.createTextNode('[RESULT]');
@@ -33,13 +42,13 @@ function Render() {
     //for in 遍歷陣列
     for (let i = 0; i < key.length; i++) {
         const ResultKey = document.createElement('li');
-        ResultKey.textContent = `KEY:  ${key[i] } 
+        ResultKey.textContent = `KEY:  ${key[i]} 
            VALUE:  ${value[i]}`;
         newDiv.appendChild(ResultKey);
     }
+    document.getElementById("Result").appendChild(resultText);
     document.getElementById("Result").appendChild(newDiv);
 }
-
 
 
 
