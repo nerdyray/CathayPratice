ef23ed02d3cc5d84800fa795808d998bb18a6063
ray <nerdyray317@gmail.com>
2025-12-15 15:56:55 +0800

    DB API OK

---
+++ Mid_Term/Q/Midterm/Midterm/src/main/java/cathaybk/Midterm/DAO/CardDAO.java
@@ -0,0 +1,43 @@
+package cathaybk.Midterm.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public class CardDAO {
+
+    private final DataSource dataSource;
+    
+    public CardDAO(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    
+    public List<Map<String,Object>> getCards() {
+        List<Map<String, Object>> cards = new ArrayList<>();
+        String SelectSql = "select id, code, name, value from poker";
+        try (Connection conn = dataSource.getConnection(); PreparedStatement ps = conn.prepareStatement(SelectSql); ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Map<String, Object> map = new LinkedHashMap<>();
+                map.put("id", rs.getInt("id"));
+                map.put("code", rs.getString("code"));
+                map.put("name", rs.getString("name"));
+                map.put("value", rs.getInt("value"));
+                cards.add(map);
+            }
+        } catch (SQLException e) {
+            System.err.println("連線測試失敗  " + e.getMessage());
+        }
+        return cards;
+    }
+}
--- Mid_Term/Q/Midterm/Midterm/src/main/java/cathaybk/Midterm/controller/MidtermController.java
+++ Mid_Term/Q/Midterm/Midterm/src/main/java/cathaybk/Midterm/controller/MidtermController.java
@@ -1,29 +1,40 @@
 package cathaybk.Midterm.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import cathaybk.Midterm.DAO.CardDAO;
 import cathaybk.Midterm.svc.MidtermService;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/midterm")
 @CrossOrigin("*")
-@RequiredArgsConstructor
 public class MidtermController {
 
     @Autowired
     MidtermService midtermService;
 
+    private final CardDAO cardDAO;
+
+    public MidtermController(CardDAO cardDAO) {
+        this.cardDAO = cardDAO;
+    }
+
+    @GetMapping(value = "/cards")
+    public List<Map<String, Object>> getCards() {
+        return cardDAO.getCards();
+    }
 
     @ResponseBody
     @PostMapping(value = "/demoCode")
@@ -36,6 +47,5 @@ public class MidtermController {
     public ResponseEntity<Map<String, Object>> submit(@RequestBody Map<String, String> map) {
         return new ResponseEntity<>(midtermService.submit(map), HttpStatus.OK);
     }
-
 
 }
